b3fb08dbb8fed8dfc43aff2f9ac4350b8337c28a
Marcelo Souza <marcelo.tadeu@mjv.com.br>
2021-05-19 09:31:36 -0300

    add project

---
+++ src/app/shared/components/like-widget/like-widget.components.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { UniqueIdService } from '../../services/unique-id/unique-id-service';
+
+@Component({
+  selector: 'app-like-widget',
+  templateUrl: './like-widget.components.html',
+  styleUrls: ['./like-widget.components.scss'],
+})
+export class LikeWidgetComponent implements OnInit {
+  @Output() public liked = new EventEmitter<void>();
+  @Input() public likes = 0;
+  @Input() public id: string = null;
+
+  public fonts = { faThumbsUp };
+
+  constructor(private uniqueIdService: UniqueIdService) {}
+
+  ngOnInit(): void {
+    if (!this.id) {
+      this.id = this.uniqueIdService.generateUniqueIdWithPrefix('like-widget');
+    }
+  }
+
+  public like(): void {
+    this.liked.emit();
+  }
+}
